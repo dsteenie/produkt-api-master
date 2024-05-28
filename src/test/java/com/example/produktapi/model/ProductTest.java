07eee9ff2501d296e75955377289008d43e0de12
deborahsteenie <d.steenie@gmail.com>
2024-05-28 17:44:38 +0100

    La till display names i ProductTest-metoden och la till test för ID - Deborah S

--- src/test/java/com/example/produktapi/model/ProductTest.java
+++ src/test/java/com/example/produktapi/model/ProductTest.java
@@ -1,7 +1,32 @@
 package com.example.produktapi.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ProductTest {
 
+    private Product product;
+
+    @BeforeEach
+    public void setUp() {
+        product = new Product("Test Product", 100.0, "Test Category", "Test Description", "Test Image");
+    }
+
+    @Test //Deborah S
+    @DisplayName("Test Get ID")
+    public void testGetId() {
+        assertNull(product.getId());
+    }
+
+    @Test //Deborah S
+    @DisplayName("Test Set ID")
+    public void testSetId() {
+        product.setId(100);
+        assertEquals(100, product.getId());
+    }
 }
+
 
