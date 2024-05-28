9b3013275a4d2ce79acafe559f8117888c3e1664
ali4mm <ali.k.mahdy@gmail.com>
2024-05-28 21:57:27 +0200

    write test code for check title

--- src/test/java/com/example/produktapi/model/ProductTest.java
+++ src/test/java/com/example/produktapi/model/ProductTest.java
@@ -3,7 +3,6 @@ package com.example.produktapi.model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ProductTest {
