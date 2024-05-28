a59e80e7bd9e0c64065f1236ed74c3b7941451ae
ali4mm <ali.k.mahdy@gmail.com>
2024-05-28 21:32:15 +0200

    write test code for check title

--- src/test/java/com/example/produktapi/model/ProductTest.java
+++ src/test/java/com/example/produktapi/model/ProductTest.java
@@ -1,7 +1,31 @@
 package com.example.produktapi.model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ProductTest {
 
-}
+    Product product;
 
+    @BeforeEach
+    public void setUp() {
+        product = new Product();
+    }
+
+    @Test
+    @DisplayName("check the get title")
+    void checkGetTitle() {
+        assertNull(product.getTitle());
+    }
+
+    @Test
+    @DisplayName("check the set title")
+    public void checkSetTitle() {
+        product.setTitle("Mens Cotton Jacket");
+
+        assertEquals("Mens Cotton Jacket", product.getTitle());
+    }
+}
