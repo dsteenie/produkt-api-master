e12193c0c93d9a0117b84e56274bce79ecd2a0c7
BornTheWrongAnimal <nataliamolina@live.se>
2024-05-28 17:53:41 +0200

    Natalia har lagt enhetstester för Product Description (Get och Set)

--- src/test/java/com/example/produktapi/model/ProductTest.java
+++ src/test/java/com/example/produktapi/model/ProductTest.java
@@ -28,6 +28,5 @@ public class ProductTest {
         product.setDescription("descriptionTestChanged");
         Assertions.assertEquals("descriptionTestChanged", product.getDescription());
     }
-
 }
 
