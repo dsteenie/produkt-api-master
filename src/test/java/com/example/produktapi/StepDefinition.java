49b7a4f435868bfdd933bba97b9fade43567925a
deborahsteenie <d.steenie@gmail.com>
2024-06-03 17:57:56 +0100

    Added dependency to fix Github Action issue, changed chromedriver input, added headless etc to StepDefinition, added Gherkin-scenario + code for Search functionality

--- src/test/java/com/example/produktapi/StepDefinition.java
+++ src/test/java/com/example/produktapi/StepDefinition.java
@@ -1,31 +1,52 @@
 package com.example.produktapi;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class StepDefinition {
 
-    static WebDriver driver;
+    private WebDriver driver;
+    private WebDriverWait wait;
 
-
-    //Beforemetod som instansierar webbdrivern - Natalia
     @Before
-    public void setup() {
-        driver = new ChromeDriver();
-        driver.manage().window().getPosition();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        this.driver = new ChromeDriver(options);
+        this.driver.manage().window().maximize();
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    //Background - Natalia
-    @Given("user opens The shop in browser")
-    public void user_opens_The_shop_in_browser() {
-        driver.get("https://webshop-agil-testautomatiserare.netlify.app/");
+    @After
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    //Background
+    @Given("I am on the homepage")
+    public void i_am_on_the_homepage() {
+        driver.get("https://webshop-agil-testautomatiserare.netlify.app");
     }
 
     @Then("title is {string}") //Natalia
@@ -33,4 +54,29 @@ public class StepDefinition {
         String actualTitle = driver.getTitle();
         Assertions.assertEquals(expectedTitle, actualTitle);
     }
+
+    //Search functionality - Deborah
+    @Given("I am on the shop page")
+    public void i_am_on_the_shop_page() {
+        driver.get("https://webshop-agil-testautomatiserare.netlify.app/products");
+    }
+
+    @When("I enter {string} into the search bar")
+    public void i_enter_into_the_search_bar(String searchQuery) {
+        WebElement searchBar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search")));
+        searchBar.sendKeys(searchQuery);
+    }
+
+    @When("I press Enter")
+    public void i_press_enter() {
+        WebElement searchBar = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("search")));
+        searchBar.sendKeys(Keys.ENTER);
+    }
+
+    @Then("I should see a product with the description {string}")
+    public void i_should_see_a_product_with_the_description(String expectedDescription) {
+        WebElement productDescription = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//p[@class='card-text' and text()='" + expectedDescription + "']")));
+        assertNotNull(productDescription, "Expected description: " + expectedDescription + " not found.");
+    }
+
 }
